f89911914b1997f1bddd2db7596d762f6e3a8f16
Trieu Vy <trtrvy1211@gmail.com>
2023-10-31 05:57:28 +0700

    call api

--- admin/controller/controller.js
+++ admin/controller/controller.js
@@ -9,11 +9,31 @@ function rennderDssp(dssp){
             <td>${data.moTa}</td>
             <td>0</td>
             <td>
-            <button class="btn btn-btn-outline-danger">Xóa</button>
-            <button class="btn-btn-primary">Sửa</button>
+            <button class="btn btn-danger">Xóa</button>
+            <button class="btn btn-primary">Sửa</button>
             </td>
         </tr>`
         contentHTML = contentHTML + trString;
+    }
+    document.getElementById('tbodySanPham').innerHTML = contentHTML;
+}
+
+function rennderProductlist(productArr){
+    var contentHTML = "";
+    for (var i = 0; i < productArr.length ; i++){   
+        var product = productArr[i];
+        var trString = `<tr>
+            <td>${product.id}</td>
+            <td>${product.name}</td>
+            <td>${product.price}</td>
+            <td>${product.img}</td>
+            <td>${product.desc}</td>
+            <td>
+            <button class="btn btn-danger">Xóa</button>
+            <button class="btn btn-primary">Sửa</button>
+            </td>
+        </tr>`
+        contentHTML += trString;
     }
     document.getElementById('tbodySanPham').innerHTML = contentHTML;
 }
