0ea277db6ac608bbbc58020e698ea6a94842fee0
Phuoc <khuathoduyphuoc2001@gmail.com>
2023-11-09 07:49:07 +0700

    Thay đổi lại giao diện phần blur

--- customer/controller/controller.js
+++ customer/controller/controller.js
@@ -113,15 +113,24 @@ domID('tblCart').innerHTML = contentHTML;
 //MÀN HÌNH MỞ KHI ẤN VÀO NÚT
 const content = domID('content');
 const cover = domID('cover');
+const coverHeader = domID('coverHeader');
+const coverFilter = domID('coverFilter');
+const coverFooter = domID('coverFooter');
 const sideNav = () => {
   content.style.transition = "0.3s";  
   content.style.transform = "translateX(-100%)";
   cover.style.transform = "translateX(0px)";
+  coverHeader.style.transform = "translateX(0px)";
+  coverFilter.style.transform = "translateX(0px)";
+  coverFooter.style.transform = "translateX(0px)";
 }
 const closes = () => {
   content.style.transition = "0.3s";
   content.style.transform ="translateX(100%)";
   cover.style.transform = "translateX(100%)";
+  coverHeader.style.transform = "translateX(100%)";
+  coverFilter.style.transform = "translateX(100%)";
+  coverFooter.style.transform = "translateX(100%)";
 }
 // function getDataForm(){
 //   let itemImg = domID('product-img').src;
